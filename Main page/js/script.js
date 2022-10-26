5568328aac2fb24ef28446da477f0d63a06bcb36
BehzodRj <behzodfarishta@gmail.com>
2022-10-26 15:08:27 +0500

    created product page

--- Main page/js/script.js
+++ Main page/js/script.js
@@ -1,8 +1,15 @@
 const dropDown = document.getElementById('dropDown')
+const dropDown2 = document.getElementById('dropDown2')
 const navbar = document.getElementById('navbar')
 
 function dropDownOpen() {
     dropDown.classList.toggle("dropDownStyle");
+    console.log(dropDown);
+}
+
+function dropDownOpen2() {
+    dropDown2.classList.toggle("dropDownStyle2");
+    console.log(dropDown2);
 }
 
 function navbarOpen() {
